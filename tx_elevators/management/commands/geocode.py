94860ce54cfe841b4c59cdf0b812dcd83dc83068
crccheck <c@crccheck.com>
2013-03-27 06:47:58 +0000

    add option to change wait duration

--- tx_elevators/management/commands/geocode.py
+++ tx_elevators/management/commands/geocode.py
@@ -12,6 +12,10 @@ class Command(BaseCommand):
         make_option('-n', '--count',
             dest='count',
             help='Only geocode this many'),
+        make_option('-w', '--wait',
+            dest='wait',
+            default=86400 / 2500,  # 2500 geolocation requests/day limit
+            help='Wait this many seconds (defaults to 35)'),
         )
 
     def handle(self, *args, **options):
@@ -21,6 +25,7 @@ class Command(BaseCommand):
 
         verbosity = int(options['verbosity'])  # default: 1
         count = options['count']
+        wait = float(options['wait'])
         qs = Building.objects.filter(latitude__isnull=True).\
             annotate(sum_floors=Sum('elevator__floors')).\
             order_by('-sum_floors')
@@ -32,7 +37,7 @@ class Command(BaseCommand):
                 print "%6d/%d %d-%s" % (i, count, building.pk, building)
             try:
                 building.geocode()
-                time.sleep(86400 / 2500)  # 2500 geolocation requests/day limit
+                time.sleep(wait)
             except KeyboardInterrupt:
                 exit(1)
             except ValueError:
