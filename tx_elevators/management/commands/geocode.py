311688d7a93d5eb4d695569ec162c970fbf1c99b
crccheck <c@crccheck.com>
2013-03-27 06:43:52 +0000

    add option to limit to N geocodes

--- tx_elevators/management/commands/geocode.py
+++ tx_elevators/management/commands/geocode.py
@@ -1,21 +1,31 @@
 from __future__ import division
 
-from django.core.management.base import NoArgsCommand
+from optparse import make_option
+
+from django.core.management.base import BaseCommand
 
 
-class Command(NoArgsCommand):
+class Command(BaseCommand):
     help = "geocode buildings."
 
-    def handle_noargs(self, *args, **options):
+    option_list = BaseCommand.option_list + (
+        make_option('-n', '--count',
+            dest='count',
+            help='Only geocode this many'),
+        )
+
+    def handle(self, *args, **options):
         import time
         from django.db.models import Sum
         from tx_elevators.models import Building
 
         verbosity = int(options['verbosity'])  # default: 1
-
+        count = options['count']
         qs = Building.objects.filter(latitude__isnull=True).\
             annotate(sum_floors=Sum('elevator__floors')).\
             order_by('-sum_floors')
+        if count is not None:
+            qs = qs[:int(count)]
         count = qs.count()
         for i, building in enumerate(qs):
             if verbosity:
