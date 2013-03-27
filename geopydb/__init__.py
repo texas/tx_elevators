32f7e3070120a96c2273f9030f89fb65367b79db
crccheck <c@crccheck.com>
2013-03-27 03:24:22 +0000

    add local disk cache of geocoded results

--- geopydb/__init__.py
+++ geopydb/__init__.py
@@ -4,17 +4,32 @@ A simple wrapper around geopy that caches to disk.
 __version__ = "0.1.0-dev"
 
 
+import anydbm
+import json
+
 from geopy import geocoders
 
 
 class GoogleV3(object):
     geocoder = None
+    store = None
 
     def __init__(self, *args, **kwargs):
         self.geocoder = geocoders.GoogleV3(*args, **kwargs)
+        self.store = anydbm.open('GoogleV3_cache', 'c')
 
-    def geocode(self, *args, **kwargs):
-        return self.geocoder.geocode(*args, **kwargs)
+    def geocode(self, address):
+        key = address
+        value = self.store.get(key, None)
+        if value is None:
+            value_raw = self.geocoder.geocode(address)
+            value = json.dumps(value_raw)
+            self.store[key] = value
+            print "value retrieved from service"
+            return value_raw
+        print "value retrieved from db"
+        value_raw = json.loads(value)
+        return value_raw
 
 
 if __name__ == "__main__":
