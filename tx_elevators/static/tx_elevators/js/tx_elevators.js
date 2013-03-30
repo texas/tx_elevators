18573599dab3eff214253c219496edfb25bf9767
crccheck <c@crccheck.com>
2013-03-30 04:17:06 +0000

    make center marker yellow/home icon

--- tx_elevators/static/tx_elevators/js/tx_elevators.js
+++ tx_elevators/static/tx_elevators/js/tx_elevators.js
@@ -25,9 +25,24 @@
       initMap(center);
     }
 
+    var pinColor = "FFFF00";
+
+    var pinImage = new google.maps.MarkerImage(
+          "http://chart.apis.google.com/chart?chst=d_map_pin_icon&chld=home|" + pinColor,
+          new google.maps.Size(21, 34),
+          new google.maps.Point(0,0),
+          new google.maps.Point(10, 34));
+
+    var pinShadow = new google.maps.MarkerImage("http://chart.apis.google.com/chart?chst=d_map_pin_shadow",
+        new google.maps.Size(40, 37),
+        new google.maps.Point(0, 0),
+        new google.maps.Point(12, 35));
+
     var marker = new google.maps.Marker({
       position: center,
-      map: map
+      map: map,
+      icon: pinImage,
+      shadow: pinShadow
     });
 
     if (buildings){
