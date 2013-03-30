19e3097a8deebaa837d4a8a41e1ddcd2bc468c20
crccheck <c@crccheck.com>
2013-03-30 05:47:00 +0000

    enable a debug flag in the html

--- example_project/settings.py
+++ example_project/settings.py
@@ -19,6 +19,9 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
+# List of internal IPs
+INTERNAL_IPS = ('127.0.0.1', '10.0.2.2', )
+
 DATABASES = {'default': dj_database_url.config(default='sqlite:///' +
     project_dir('example_project.sqlite'))}
 
