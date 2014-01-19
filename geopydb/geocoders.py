0d8c42706627101e5a122b1b41923ecb4b0560b2
crccheck <c@crccheck.com>
2014-01-19 13:31:46 -0600

    convert geocoder to use logging instead of print

--- geopydb/geocoders.py
+++ geopydb/geocoders.py
@@ -1,5 +1,6 @@
 import anydbm
 import json
+import logging
 
 from geopy import geocoders
 
@@ -16,6 +17,7 @@ class GoogleV3(object):
 
             force: force a lookup, bypassing the cache
         """
+        logger = logging.getLogger(__name__)
         key = query
         store = anydbm.open('GoogleV3_cache', 'c')
         value = store.get(key, None)
@@ -25,7 +27,12 @@ class GoogleV3(object):
                 value = json.dumps(value_raw)
                 store[key] = value
                 store.close()
-                print "value retrieved from service"
+                logger.info('value retrieved from service', extra=dict(kwargs,
+                    query=query,
+                    force=force,
+                    key=key,
+                    value=key,
+                ))
                 return value_raw
             except (ValueError, geocoders.base.GeocoderResultError) as e:
                 value = str(e)
@@ -33,7 +40,12 @@ class GoogleV3(object):
                 store.close()
                 raise e
         store.close()
-        print "value retrieved from db"
+        logger.info('value retrieved from db', extra=dict(kwargs,
+            query=query,
+            force=force,
+            key=key,
+            value=key,
+        ))
         try:
             value_raw = json.loads(value)
         except ValueError:
