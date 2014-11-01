acc60d642217885cf215a1f09196fa406500a53f
crccheck <c@crccheck.com>
2014-11-01 14:08:46 -0500

    add a management command for dumping saved geo data

---
+++ tx_elevators/management/commands/dumpgeo.py
@@ -0,0 +1,15 @@
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    help = 'dump geo data'
+
+    def handle(self, *args, **options):
+        from tx_elevators.models import Building
+
+        for x in Building.objects.filter(latitude__isnull=False):
+            print('{elbi},{latitude},{longitude}'.format(**x.__dict__))
